0387455b0fd795704105ed132e21307db5aca910
Kero9811 <kero9811@naver.com>
2024-03-22 05:42:16 +0900

    save

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    IDLE,
+    MOVE,
+    DASH,
+    JUMP,
+    FALL,
+    ATTACK,
+    HIT,
+    GROUNDSMASH,
+    DEAD
+}
+
+public class Player : MonoBehaviour
+{
+    
+}
